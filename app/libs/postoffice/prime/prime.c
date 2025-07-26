9d1cb6920269997a5ecdaffe064983bb742ae85e
Seintian <chrstnsntrll@gmail.com>
2025-07-26 17:06:33 +0200

    Reorganizes library directories and updates Makefile paths
    Introduces `postoffice` and `thirdparty` subdirectories within the `libs` folder to better separate internal and external libraries. Updates Makefile paths and variables accordingly to reflect this new structure.
    
    Adds support for new libraries (`lmdb` and `mbedtls`), adjusts include and source file resolution, and updates object file generation rules. Removes deprecated library files and headers to streamline the project.
    
    Enhances maintainability by improving folder structure and simplifying library management.

---
+++ app/libs/postoffice/prime/prime.c
@@ -0,0 +1,45 @@
+/**
+ * @file prime.c
+ * @brief Implementation of functions for prime number operations.
+ * 
+ * @see prime.h
+ */
+
+#include <stdlib.h>
+#include "prime/prime.h"
+
+
+bool is_prime(size_t n) {
+    if (n <= 1)
+        return false;
+
+    if (n <= 3)
+        return true;
+
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (size_t i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+
+size_t next_prime(size_t n) {
+    if (n <= 1)
+        return 2;
+
+    size_t prime = n;
+    size_t found = 0;
+
+    while (!found) {
+        prime++;
+
+        if (is_prime(prime))
+            found = 1;
+    }
+
+    return prime;
+}
