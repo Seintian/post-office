4084dc9143c1c0a6273a2273a437ae92cc91eed6
Seintian <chrstnsntrll@gmail.com>
2025-08-31 23:08:32 +0200

    Fix unit test naming conventions for PERF tests

--- app/tests/perf/test_perf.c
+++ app/tests/perf/test_perf.c
@@ -38,26 +38,26 @@ TEST_TEAR_DOWN(PERF) {
     CAPTURE_REPORT(buf, sizeof(buf), perf_shutdown);
 }
 
-TEST(PERF, InitAndShutdown) {
+TEST(PERF, INITANDSHUTDOWN) {
     TEST_ASSERT_EQUAL_INT(0, perf_init(4, 2, 1));
     char buf[2048];
     CAPTURE_REPORT(buf, sizeof(buf), perf_shutdown);
     TEST_ASSERT_EQUAL_INT(0, perf_init(1, 1, 1)); // reinit works
 }
 
-TEST(PERF, DoubleInitError) {
+TEST(PERF, DOUBLEINITERROR) {
     TEST_ASSERT_EQUAL_INT(0, perf_init(1, 1, 1));
     TEST_ASSERT_EQUAL_INT(-1, perf_init(1, 1, 1));
     TEST_ASSERT_EQUAL_INT(PERF_EALREADY, errno);
 }
 
-TEST(PERF, CounterBeforeInit) {
+TEST(PERF, COUNTERBEFOREINIT) {
     perf_shutdown(stdout);
     TEST_ASSERT_EQUAL_INT(-1, perf_counter_create("c"));
     TEST_ASSERT_EQUAL_INT(PERF_ENOTINIT, errno);
 }
 
-TEST(PERF, CounterCreateAndIncrement) {
+TEST(PERF, COUNTERCREATEANDINCREMENT) {
     TEST_ASSERT_EQUAL_INT(0, perf_init(2, 2, 2));
     TEST_ASSERT_NOT_EQUAL_INT(-1, perf_counter_create("ct"));
 
@@ -74,13 +74,13 @@ TEST(PERF, CounterCreateAndIncrement) {
     TEST_ASSERT_NOT_NULL(strstr(buf, "ct: 4")); // 1+3
 }
 
-TEST(PERF, TimerBeforeInit) {
+TEST(PERF, TIMERBEFOREINIT) {
     perf_shutdown(stdout);
     TEST_ASSERT_EQUAL_INT(-1, perf_timer_create("t"));
     TEST_ASSERT_EQUAL_INT(PERF_ENOTINIT, errno);
 }
 
-TEST(PERF, TimerCreateAndMeasure) {
+TEST(PERF, TIMERCREATEANDMEASURE) {
     perf_init(1, 1, 1);
     TEST_ASSERT_EQUAL_INT(0, perf_timer_create("tm"));
 
@@ -97,13 +97,13 @@ TEST(PERF, TimerCreateAndMeasure) {
     TEST_ASSERT_NOT_NULL(strstr(buf, "tm:")); // value > 0 printed
 }
 
-TEST(PERF, HistogramBeforeInit) {
+TEST(PERF, HISTOGRAMBEFOREINIT) {
     perf_shutdown(stdout);
     uint64_t bins[] = {10, 20};
     TEST_ASSERT_EQUAL_INT(-1, perf_histogram_create("h", bins, 2));
 }
 
-TEST(PERF, HistogramCreateAndRecordBins) {
+TEST(PERF, HISTOGRAMCREATEANDRECORDBINS) {
     perf_init(1, 1, 1);
     uint64_t bins[] = {5, 15, 30};
     TEST_ASSERT_EQUAL_INT(0, perf_histogram_create("hg", bins, 3));
@@ -123,7 +123,7 @@ TEST(PERF, HistogramCreateAndRecordBins) {
     TEST_ASSERT_NOT_NULL(strstr(buf, "<= 30: 1"));
 }
 
-TEST(PERF, HistogramOverflowBin) {
+TEST(PERF, HISTOGRAMOVERFLOWBIN) {
     perf_init(1, 1, 1);
     uint64_t bins[] = {1, 2};
     TEST_ASSERT_EQUAL_INT(0, perf_histogram_create("of", bins, 2));
@@ -140,13 +140,13 @@ TEST(PERF, HistogramOverflowBin) {
 }
 
 TEST_GROUP_RUNNER(PERF) {
-    RUN_TEST_CASE(PERF, InitAndShutdown);
-    RUN_TEST_CASE(PERF, DoubleInitError);
-    RUN_TEST_CASE(PERF, CounterBeforeInit);
-    RUN_TEST_CASE(PERF, CounterCreateAndIncrement);
-    RUN_TEST_CASE(PERF, TimerBeforeInit);
-    RUN_TEST_CASE(PERF, TimerCreateAndMeasure);
-    RUN_TEST_CASE(PERF, HistogramBeforeInit);
-    RUN_TEST_CASE(PERF, HistogramCreateAndRecordBins);
-    RUN_TEST_CASE(PERF, HistogramOverflowBin);
+    RUN_TEST_CASE(PERF, INITANDSHUTDOWN);
+    RUN_TEST_CASE(PERF, DOUBLEINITERROR);
+    RUN_TEST_CASE(PERF, COUNTERBEFOREINIT);
+    RUN_TEST_CASE(PERF, COUNTERCREATEANDINCREMENT);
+    RUN_TEST_CASE(PERF, TIMERBEFOREINIT);
+    RUN_TEST_CASE(PERF, TIMERCREATEANDMEASURE);
+    RUN_TEST_CASE(PERF, HISTOGRAMBEFOREINIT);
+    RUN_TEST_CASE(PERF, HISTOGRAMCREATEANDRECORDBINS);
+    RUN_TEST_CASE(PERF, HISTOGRAMOVERFLOWBIN);
 }
