fa5906004a7684aa5690dc272f43481c9c038fcc
Seintian <chrstnsntrll@gmail.com>
2025-09-03 08:03:49 +0200

    Deprecate and remove old sysinfo module files

--- app/libs/postoffice/core/simulation/sysinfo/fsinfo.h
+++ app/libs/postoffice/core/simulation/sysinfo/fsinfo.h
@@ -1,12 +1,3 @@
-#ifndef _FSINFO_H
-#define _FSINFO_H
-
-#include <stddef.h>
-#include <stdio.h>
-
-
-unsigned long free_disk_space(const char *path) __nonnull((1));
-
-int get_fs_type(const char *path, char *fs_type, size_t size) __nonnull((1, 2));
-
-#endif // _FSINFO_H
+// Deprecated shim: include new header
+#pragma once
+#include "sysinfo/fsinfo.h"
--- app/libs/postoffice/core/simulation/sysinfo/hugeinfo.h
+++ app/libs/postoffice/core/simulation/sysinfo/hugeinfo.h
@@ -1,15 +1,3 @@
-#ifndef _HUGEINFO_H
-#define _HUGEINFO_H
-
-#include <stddef.h>
-#include "core/simulation/sysinfo.h"
-
-
-// Returns number of supported sizes, fills sizes[k] (caller allocates).
-// Returns -1 on error.
-int list_hugepage_sizes(unsigned long *sizes_kB, size_t max, size_t *count);
-
-// Fills *info for given size (in kB). Returns 0 on success, -1 on error.
-int get_hugepage_info(unsigned long size_kB, hugepage_info_t *info);
-
-#endif // _HUGEINFO_H
+// Deprecated shim: include new header
+#pragma once
+#include "sysinfo/hugeinfo.h"
